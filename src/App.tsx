4da289f63f56df702f62ad7e3a10108caffd6213
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-10-25 23:32:59 +0500

    setted up vite project & tailwind css

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return <></>;
+}
+
+export default App;
+
+{
+  // ! the ways how to import those things
+  // import reactLogo from "./assets/react.svg";
+  // import viteLogo from "/vite.svg";
+  // import "./App.css";
+}
