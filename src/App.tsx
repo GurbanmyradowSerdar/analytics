045016e25710e4b15d96078e0d6aa5b69d694cf8
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2023-11-01 00:58:36 +0500

    added mobile page (not available)

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Footer from "./components/Footer";
 import FirstHomeSection from "./components/home/sections/FirstHomeSection";
 import SecondHomeSection from "./components/home/sections/SecondHomeSection";
 import ThirdHomeSection from "./components/home/sections/ThirdHomeSection";
 import FourthHomeSection from "./components/home/sections/FourthHomeSection";
 import Header from "./components/Header";
+import MobilePage from "./components/mobile";
 
 const navItems: string[] = ["Product", "Pricing Plans", "FAQ", "Blog"];
 export const AppContext = createContext<string[]>(navItems);
 
 function App() {
-  return (
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  return windowWidth <= 1020 ? (
+    <MobilePage />
+  ) : (
     <div className="font-Roboto flex flex-col text-text-secondary-color min-h-screen">
       <main className="flex-grow">
         <FirstHomeSection>
---
+++ src/components/mobile/index.tsx
@@ -0,0 +1,21 @@
+const MobilePage = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="text-3xl text-center px-10 flex flex-col gap-5 items-center justify-center">
+        {/* <div className="w-20 h-20 bg-text-main-color rounded-full animate-bounce"></div> */}
+        <h1>
+          This website is not available on{" "}
+          <span className="text-blue-700 font-medium">Mobile devices</span>
+        </h1>
+        <a
+          href="https://github.com/GurbanmyradowSerdar"
+          className="underline text-blue-950"
+        >
+          Creator
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default MobilePage;
