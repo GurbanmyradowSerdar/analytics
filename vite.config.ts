f2972207c11d697e256570fe79efdbbfa036d953
Serdar <115923465+GurbanmyradowSerdar@users.noreply.github.com>
2024-05-21 21:58:43 +0500

    chore(tsconfig.json): add absolute paths of main folders (hooks,components) to upgrade DX;

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: [
+      { find: "@components", replacement: "/src/components" },
+      { find: "@hooks", replacement: "/src/hooks" },
+      { find: "@types", replacement: "/src/types/index.ts" },
+      { find: "@utils", replacement: "/src/utils" },
+      { find: "@src", replacement: "/src" },
+    ],
+  },
+});
